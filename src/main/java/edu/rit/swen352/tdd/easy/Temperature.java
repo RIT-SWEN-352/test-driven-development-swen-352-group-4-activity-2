4c3f754a18258d4aa008557de647555bd6f386b7
Griffin Vella <griffinvella@gmail.com>
2026-06-27 18:49:30 -0400

    Converting temperature from  -100 celsius to kelvin failing because of double precision, going to add tolerence to test

--- src/main/java/edu/rit/swen352/tdd/easy/Temperature.java
+++ src/main/java/edu/rit/swen352/tdd/easy/Temperature.java
@@ -50,7 +50,36 @@ public class Temperature {
   }
 
   public Temperature convertTo(TemperatureUnit newUnit) {
-    return new Temperature(this.value, newUnit);
+    if (unit == newUnit) {
+      return new Temperature(value, unit);
+    }
+
+    double newValue = 0;
+
+    switch (unit) {
+      case CELSIUS:
+        if (newUnit == TemperatureUnit.FAHRENHEIT) {
+          newValue = value * 9 / 5 + 32;
+        } else if (newUnit == TemperatureUnit.KELVIN) {
+          newValue = value + 273.15;
+        }
+        break;
+      case FAHRENHEIT:
+        if (newUnit == TemperatureUnit.CELSIUS) {
+          newValue = (value - 32) * 5 / 9;
+        } else if (newUnit == TemperatureUnit.KELVIN) {
+          newValue = (value - 32) * 5 / 9 + 273.15;
+        }
+        break;
+      case KELVIN:
+        if (newUnit == TemperatureUnit.CELSIUS) {
+          newValue = value - 273.15;
+        } else if (newUnit == TemperatureUnit.FAHRENHEIT) {
+          newValue = (value - 273.15) * 9 / 5 + 32;
+        }
+        break;
+    }
+    return new Temperature(newValue, newUnit);
   }
 
 }
