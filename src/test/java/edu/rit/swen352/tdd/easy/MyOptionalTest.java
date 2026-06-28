9abb2578039b50413fa70b48b30dbde87650996b
Griffin Vella <griffinvella@gmail.com>
2026-06-27 22:21:42 -0400

    empty: test empty optional  starts empty : added failing test

--- src/test/java/edu/rit/swen352/tdd/easy/MyOptionalTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/MyOptionalTest.java
@@ -9,5 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test suite for the {@link MyOptional} component.
  */
 class MyOptionalTest {
-
+    @Test
+    @DisplayName("Test that MyOptional is empty when created with empty()")
+    void testEmptyOptional() {
+        MyOptional<String> optional = MyOptional.empty();
+        assertFalse(optional.isPresent(), "Optional should be empty");
+    }
+    
 }
