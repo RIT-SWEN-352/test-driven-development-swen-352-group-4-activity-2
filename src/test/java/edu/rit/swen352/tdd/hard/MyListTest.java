93ed3f87bda3ff7d0ea78e4bdd0d6b4fdb69f2e4
Griffin Vella <griffinvella@gmail.com>
2026-06-27 22:48:04 -0400

    ctor : Test that MyList is empty when created : added failing test

--- src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
@@ -9,5 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test suite for the {@link MyList} component.
  */
 class MyListTest {
+    @Test
+    @DisplayName("Test that MyList is empty when created")
+    void testEmptyList() {
+        MyList<String> list = new MyList<>(10);
+        assertTrue(list.isEmpty(), "List should be empty");
+    }
 
 }
