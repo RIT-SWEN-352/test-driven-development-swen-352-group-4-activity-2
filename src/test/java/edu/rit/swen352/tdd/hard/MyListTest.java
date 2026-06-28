5546b52d6227a47866e5064f914cabbaa8753c5e
Griffin Vella <griffinvella@gmail.com>
2026-06-27 23:37:10 -0400

    remove : Test that remove correctly removes elements : modified the test to work with return returning the value removed instead of a boolean. - still fails

--- src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
@@ -75,7 +75,7 @@ class MyListTest {
         list.add(1);
         list.add(2);
         list.add(3);
-        assertTrue(list.remove(2), "remove should return True after succesfully removing an element");
+        assertEquals(2, list.remove(2), "remove should return the removed element (2)");
         assertAll(
             () -> assertEquals(3, list.get(2), "After removing the element at index 2 the remainings elements (3) move to fill the gap"),
             () -> assertEquals(2, list.size(), "after removing an element, the size should be reduced by 1");
