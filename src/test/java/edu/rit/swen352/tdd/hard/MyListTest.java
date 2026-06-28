55a6db115dae4b5d55d196cce1711b3286977818
Griffin Vella <griffinvella@gmail.com>
2026-06-27 23:38:46 -0400

    remove - Test that remove correctly removes elements - removed random typo in test

--- src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/MyListTest.java
@@ -78,7 +78,7 @@ class MyListTest {
         assertEquals(2, list.remove(2), "remove should return the removed element (2)");
         assertAll(
             () -> assertEquals(3, list.get(2), "After removing the element at index 2 the remainings elements (3) move to fill the gap"),
-            () -> assertEquals(2, list.size(), "after removing an element, the size should be reduced by 1");
+            () -> assertEquals(2, list.size(), "after removing an element, the size should be reduced by 1")
         );
     }
 
